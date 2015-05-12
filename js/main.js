fef54c6f293f7530de29988d5fd3e204f8129dad
David James <d.james88@btinternet.com>
2015-05-12 22:54:41 +0100

    styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 var board = []; //array that represents the board and stores the players moves
-// var board = {};
 var currentMove ; //current move switches between player1 & player2
-// var lastMove= true;
 var player1 = 'X' //player1 token
 var player2 = 'O' //player2 token
 var movesMade = 0;//moves made// maximum of 9
@@ -12,6 +10,7 @@ $( document ).ready(function() {
 
   function updateBoard(){          //this function updates the board array
     $('.box').on('click', function(){ //event listening for a click
+      $(this).unbind('click'); 
       var playMove = Number(this.id) //playerMove equal to to id of the clicked position
       if (movesMade % 2 === 0) {   //condition thats decideds if player one moves
         currentMove = player1;
@@ -22,22 +21,23 @@ $( document ).ready(function() {
       board[playMove] = currentMove;//puts player1or2 move into the correct poistion in the array
       movesMade ++; //increments movesMade by one each time 
       $(this).html(currentMove);//prints player move on hmtl //TEMPORARY//
-      getWinner();
+      getWinner();//calls the getWinner function
     });
-  }
+  }//end of Updateboard
 updateBoard();
 
   function getWinner(){
     $.each(winCombinations, function(index, value){
       
       if (board[value[0]] === board[value[1]] && board[value[0]] === board[value[2]] && board[value[0]] !== undefined && board[value[2]] !== undefined ){
-        alert('winner');
+        // debugger;
+        console.log(currentMove + " wins!");
       }
-     })   
-  };
-
-
-
+      // else if (movesMade === 9){
+      //   console.log('Tie!')
+      // }
+     })
+  };//end of getWinner function//
 });//document.ready
 
   
