584ec58c4ce9cbed4868e682862e910791c92ed6
David James <d.james88@btinternet.com>
2015-05-14 09:51:05 +0100

    refresh function

--- js/main.js
+++ js/main.js
@@ -3,41 +3,113 @@ var currentMove ; //current move switches between player1 & player2
 var player1 = 'X' //player1 token
 var player2 = 'O' //player2 token
 var movesMade = 0;//moves made// maximum of 9
- var winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+var winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
                       [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+var winner = ''; //used for getTie function
 
-$( document ).ready(function() {
+$( document ).ready(function() {  
+
+  soundManager.setup({
+    url: 'sounds/',
+    flashVersion: 9,
+    preferFlash: false,
+    onready: function() {
+      var background = soundManager.createSound({
+        url: 'sounds/background.mp3',
+        id: 'background'
+      });
+      background.play();
+    }      
+  });//end of soundmanager background track//
+
+  // set up event listener
+  $('.box').on('click', updateBoard);
+   $('#reset').on('click', resetGame);
+    $('#mute').on('click', toggleMute); 
+ 
+   function toggleMute(){
+      soundManager.toggleMute('background');
+     
+  }
+
 
   function updateBoard(){          //this function updates the board array
-    $('.box').on('click', function(){ //event listening for a click
-      $(this).unbind('click'); 
-      var playMove = Number(this.id) //playerMove equal to to id of the clicked position
-      if (movesMade % 2 === 0) {   //condition thats decideds if player one moves
-        currentMove = player1;
-      }
-      else {
-        currentMove = player2;// player2 moves
-      }
-      board[playMove] = currentMove;//puts player1or2 move into the correct poistion in the array
-      movesMade ++; //increments movesMade by one each time 
-      $(this).html(currentMove);//prints player move on hmtl //TEMPORARY//
-      getWinner();//calls the getWinner function
-    });
+    
+     var playMove = Number(this.id) //playerMove equal to to id of the clicked position
+     if (movesMade % 2 === 0) {   //condition thats decideds if player one moves
+       currentMove = player1;
+         $('p').css('color','#B6FF00');
+     }
+    else {
+       currentMove = player2;// player2 moves
+         $('p').css('color','#FF1177');
+     }
+    board[playMove] = currentMove;//puts player1or2 move into the correct poistion in the array
+    movesMade = movesMade+1; //increments movesMade by one each time 
+    $(this).children(':first').html(currentMove);//prints player move on hmtl 
+    //$(this).off('click'); 
+    $(this).removeClass("box").addClass("box2");
+    getWinner();//calls the getWinner function
+    getTie();//calls the getTie function
+    
+    console.log(currentMove, movesMade)
+
   }//end of Updateboard
-updateBoard();
+
 
   function getWinner(){
     $.each(winCombinations, function(index, value){
-      
       if (board[value[0]] === board[value[1]] && board[value[0]] === board[value[2]] && board[value[0]] !== undefined && board[value[2]] !== undefined ){
-        // debugger;
-        console.log(currentMove + " wins!");
+        if (currentMove === 'X') {
+          winner = true;
+          $('h1').text('PLAYER 1 WINS!'); 
+          // resetGame();
+        }
+        else if (currentMove === 'O'){
+          $('h1').text('PLAYER 2 WINS');
+          winner = true;
+        }
       }
-      // else if (movesMade === 9){
-      //   console.log('Tie!')
-      // }
      })
+
   };//end of getWinner function//
+
+  function getTie(){
+    if (movesMade === 9 && winner !== true) {
+      $('h1').text('Tie Game!');
+    } 
+    
+  }///end of getTie function//
+
+
+function resetGame(){
+    board = [];
+    currentMove = '';
+    movesMade = 0;
+    winner = ''; 
+    $('h1').text('TicTacToe')
+    $('.box').html(''); 
+    //$('.box').on('click', updateBoard)
+    $('p').html('');
+    $.each($('div'), function(index, value){
+      //debugger;
+    if ($(value).hasClass('box2')){
+    $(value).removeClass("box2").addClass("box");
+     }
+   });
+
+    //debugger;
+  
+  }//end of reset function//
+
 });//document.ready
+
+
+
+
+
+
+
+
 
   
